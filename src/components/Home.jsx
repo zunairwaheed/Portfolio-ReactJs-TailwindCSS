75c3a7edbf1d909b5c83b91ca3336f4172cfe557
zunairwaheed <zunairwaheed125@gmail.com>
2026-01-05 16:17:48 +0500

    update: home image

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import { FaInstagramSquare, FaLinkedin, FaGithub, FaReact, FaNodeJs } from "react-icons/fa";
 import { SiMongodb, SiExpress } from "react-icons/si";
 import { ReactTyped } from "react-typed";
+import me from "../assets/me.png";
 
 function Home() {
   return (
@@ -98,7 +99,7 @@ function Home() {
           {/* Profile Image */}
           <div className="w-full md:w-1/2 flex justify-center md:justify-end">
             <img
-              src="https://media.licdn.com/dms/image/v2/D4D03AQFSynekkrgKsQ/profile-displayphoto-shrink_800_800/B4DZVB.TLzG8Ac-/0/1740568614383?e=1764806400&v=beta&t=SYtrAeoqQZesaApj1dQZue8G_H3xb9sxVjrHPurOUc8"
+              src={me}
               alt="Profile"
               className="w-48 h-48 md:w-[400px] md:h-[400px] object-cover rounded-full border-4 border-gray-300 shadow-md"
             />
